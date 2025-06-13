0eecdfc75fbe65894ee063a1894b683af9b88044
Blackthifer <hessel96@msn.com>
2025-06-13 15:32:35 +0200

    adds file writing tests and fixes file writing

--- functions/write_file.py
+++ functions/write_file.py
@@ -3,14 +3,15 @@ import os
 def write_file(working_directory, file_path, content):
     f_path = os.path.realpath(os.path.join(os.path.abspath(working_directory), file_path))
     wd_path = os.path.abspath(working_directory)
-    if not os.path.exists(f_path):
-        os.makedirs(f_path)
-    if not os.path.isfile(f_path):
-        return f'Error: Path is not a regular file: "{file_path}"'
     if wd_path not in f_path:
         return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not os.path.exists(f_path):
+        os.makedirs(os.path.split(f_path)[0], exist_ok=True)
+        open(f_path, "w").close()
+    if not os.path.isfile(f_path):
+        return f'Error: Path is not a regular file: "{file_path}"'
     try:
-        file = open(file_path, "w")
+        file = open(f_path, "w")
         written = file.write(content)
         file.close()
     except Exception as e:
--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import unittest
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
 class TestFiles(unittest.TestCase):
     def test_various(self):
@@ -8,10 +9,13 @@ class TestFiles(unittest.TestCase):
         #print(get_files_info("calculator", "pkg"))
         #print(get_files_info("calculator", "/bin"))
         #print(get_files_info("calculator", "../"))
-        print(get_file_content("calculator", "lorem.txt"))
-        print(get_file_content("calculator", "main.py"))
-        print(get_file_content("calculator", "pkg/calculator.py"))
-        print(get_file_content("calculator", "/bin/cat"))
+        #print(get_file_content("calculator", "lorem.txt"))
+        #print(get_file_content("calculator", "main.py"))
+        #print(get_file_content("calculator", "pkg/calculator.py"))
+        #print(get_file_content("calculator", "/bin/cat"))
+        print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
+        print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+        print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
 
 if __name__ == "__main__":
     unittest.main()
