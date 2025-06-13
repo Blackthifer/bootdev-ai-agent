37d58e49a57ba48cb86b0c2e6287928731895966
Blackthifer <hessel96@msn.com>
2025-06-13 14:47:27 +0200

    adds file info tests and fixes file info getter

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,15 +1,19 @@
 import os
 
 def get_files_info(working_directory, directory=None):
-    if not directory.startswith(working_directory):
-        return f'Error: cannot list "{directory}" as it is outside the permitted working directory'
-    if not os.path.isdir(directory) or not os.path.exists(directory):
+    if directory is None:
         return f'Error: "{directory}" is not a directory'
+    d_path = os.path.realpath(os.path.join(os.path.abspath(working_directory), directory))
+    wd_path = os.path.abspath(working_directory)
+    if not os.path.isdir(d_path) or not os.path.exists(d_path):
+        return f'Error: "{directory}" is not a directory'
+    if wd_path not in d_path:
+        return f'Error: cannot list "{directory}" as it is outside the permitted working directory'
     try:
-        dir_contents = os.listdir(directory)
+        dir_contents = os.listdir(d_path)
         output = ""
         for content in dir_contents:
-            content_path = os.path.join(directory, content)
+            content_path = os.path.join(d_path, content)
             output += f"{content}: file_size={os.path.getsize(content_path)} bytes, is_dir={os.path.isdir(content_path)}\n"
     except Exception as e:
         return f"Error: {e}"
